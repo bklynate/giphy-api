78a969025fe5c7bd9efb2f6b9707e431b53ecb0d
bklynate <nathanielcfa@gmail.com>
2016-12-18 01:11:09 -0500

    working on displaying wordbank

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
-var topics = ['chappelle','desus and mero','medical marijuana', 'whiskey', 'saitama', '']
+var topics = ['chappelle','desus and mero','medical marijuana', 'whiskey', 'saitama']
 
-var url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
-'
+var url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC'
 
 topics.forEach(function(item){
-  console.log(item);
-})
+  button = $('<button>');
+  button.addClass("btn btn-success");
+  button.text(item)
+  $('#buttonBank').append(button)
+});
