dfd6e3d79a82c8b2586a57f2614078d46a464cbb
bklynate <nathanielcfa@gmail.com>
2016-12-18 00:30:25 -0500

    added heroku deployment, index and js structuring

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var topics = ['chappelle','desus and mero','medical marijuana', 'whiskey', 'saitama', '']
+
+var url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+'
+
+topic.forEach(function(item){
+  console.log(item);
+})
